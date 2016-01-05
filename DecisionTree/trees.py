f27bcde2598b2232b36423b0f58432ea42b8646f
陈斌 <chenbkeep@163.com>
2016-01-05 20:46:47 +0800

    创建树图形

--- DecisionTree/trees.py
+++ DecisionTree/trees.py
@@ -95,6 +95,31 @@ def create_tree(data_set, labels):
     return my_tree
 
 
+def classify(inputTree,featLabels,testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    key = testVec[featIndex]
+    valueOfFeat = secondDict[key]
+    if isinstance(valueOfFeat, dict):
+        classLabel = classify(valueOfFeat, featLabels, testVec)
+    else: classLabel = valueOfFeat
+    return classLabel
+
+
+def storeTree(inputTree,filename):
+    import pickle
+    fw = open(filename,'w')
+    pickle.dump(inputTree,fw)
+    fw.close()
+
+
+def grabTree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
+
+
 a, b = create_data_set()
 print a
 # print calc_shannon_ent(a)
