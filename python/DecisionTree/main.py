4344ae4b2ede4fad70674f2045da4a782f39e3b8
chenbin <chenbkeep@163.com>
2016-03-19 22:18:55 +0800

    python

--- python/DecisionTree/main.py
+++ python/DecisionTree/main.py
@@ -1,8 +1,13 @@
-import treePlotter
 import trees
 
 a, b = trees.create_data_set()
-print a
-tree = trees.create_tree(a, b)
-print tree
+print(a)
+
+# import treePlotter
+# import trees
+#
+# a, b = trees.create_data_set()
+# print(a)
+# tree = trees.create_tree(a, b)
+# print tree
 # treePlotter.create_plot(tree)
