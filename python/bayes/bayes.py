e9a9daff6a4b705a2616f7df1dc23b813e17fafb
陈斌 <chenbkeep@163.com>
2016-03-30 10:57:20 +0800

    垃圾邮件分类

--- python/bayes/bayes.py
+++ python/bayes/bayes.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from numpy import *
 
 def loadDataSet():
@@ -25,8 +26,8 @@ def setOfWords2Vec(vocabList, inputSet):
     return returnVec
 
 def trainNB0(trainMatrix,trainCategory):
-    numTrainDocs = len(trainMatrix)
-    numWords = len(trainMatrix[0])
+    numTrainDocs = len(trainMatrix) # articles number
+    numWords = len(trainMatrix[0])  #words number
     pAbusive = sum(trainCategory)/float(numTrainDocs)
     p0Num = ones(numWords); p1Num = ones(numWords)      #change to ones()
     p0Denom = 2.0; p1Denom = 2.0                        #change to 2.0
@@ -40,3 +41,71 @@ def trainNB0(trainMatrix,trainCategory):
     p1Vect = log(p1Num/p1Denom)          #change to log()
     p0Vect = log(p0Num/p0Denom)          #change to log()
     return p0Vect,p1Vect,pAbusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)    #element-wise mult
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+#词袋模型
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
+def testingNB():
+    listOPosts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+
+
+def textParse(bigString):    #input is big string, #output is word list
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList=[]; classList = []; fullText =[]
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt' % i,encoding='gbk').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i,encoding='utf-8').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)#create vocabulary
+    trainingSet = range(50); testSet=[]           #create test set
+    for i in range(10):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(list(trainingSet)[randIndex])
+    trainMat=[]; trainClasses = []
+    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:        #classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount += 1
+            print("classification error",docList[docIndex])
+    print('the error rate is: ',float(errorCount)/len(testSet))
+    #return vocabList,fullText
--- python/bayes/main.py
+++ python/bayes/main.py
@@ -2,22 +2,25 @@ from numpy import *
 
 import bayes
 
-listOPosts,listClasses=bayes.loadDataSet()
-myVocabList=bayes.createVocabList(listOPosts)
+# listOPosts,listClasses=bayes.loadDataSet()
+# myVocabList=bayes.createVocabList(listOPosts)
+#
+# trainMat=[]
+# for postinDoc in listOPosts:
+#     trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
 
-trainMat=[]
-for postinDoc in listOPosts:
-    trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
+# print(trainMat)
+# print(listClasses)
 
-print(trainMat)
+# p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
 
-p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
-
-print(pAb)
-print(p0V)
-print(p1V)
+# print(pAb)
+# print(p0V)
+# print(p1V)
 
 # a=bayes.setOfWords2Vec(myVocabList,listOPosts[0])
 # print(listOPosts)
 # print(myVocabList)
 # print(a)
+
+bayes.spamTest()
