22b78ea398baf987d018cca1b446d72aa169ce1d
陈斌 <chenbkeep@163.com>
2016-03-30 09:27:52 +0800

    贝叶斯

---
+++ python/bayes/main.py
@@ -0,0 +1,23 @@
+from numpy import *
+
+import bayes
+
+listOPosts,listClasses=bayes.loadDataSet()
+myVocabList=bayes.createVocabList(listOPosts)
+
+trainMat=[]
+for postinDoc in listOPosts:
+    trainMat.append(bayes.setOfWords2Vec(myVocabList,postinDoc))
+
+print(trainMat)
+
+p0V,p1V,pAb=bayes.trainNB0(trainMat,listClasses)
+
+print(pAb)
+print(p0V)
+print(p1V)
+
+# a=bayes.setOfWords2Vec(myVocabList,listOPosts[0])
+# print(listOPosts)
+# print(myVocabList)
+# print(a)
