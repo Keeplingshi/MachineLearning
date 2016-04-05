63c6877da6ab4ae62005bc1b4e51b321b84d8b2b
陈斌 <chenbkeep@163.com>
2016-04-05 10:09:32 +0800

    矩阵数据处理，元素0-1

--- matlab/spectralClustering/main.m
+++ matlab/spectralClustering/main.m
@@ -4,19 +4,22 @@ clear
 
 %twoCircles数据集
 load('twoCircles.mat');
-dataSet=twoCircles;
+dataSetOri=twoCircles;
+dataSet=dataSetOri/max(max(abs(dataSetOri)));
 num_clusters=2;
-sigma=0.05;
+sigma=0.1;
 
 %XOR数据集
 % load('XOR.mat');
-% dataSet=XOR;
+% dataSetOri=XOR;
+% dataSet=dataSetOri/max(max(abs(dataSetOri)));
 % num_clusters=4;
-% sigma=10;
+% sigma=0.1;
+
 
 Z=pdist(dataSet);
 W=squareform(Z);
 
 C = spectral(W,sigma, num_clusters);
 
-plot(dataSet(C==1,1),dataSet(C==1,2),'r.', dataSet(C==2,1),dataSet(C==2,2),'b.', dataSet(C==3,1),dataSet(C==3,2),'g.', dataSet(C==4,1),dataSet(C==4,2),'m.');
+plot(dataSetOri(C==1,1),dataSetOri(C==1,2),'r.', dataSetOri(C==2,1),dataSetOri(C==2,2),'b.', dataSetOri(C==3,1),dataSetOri(C==3,2),'g.', dataSetOri(C==4,1),dataSetOri(C==4,2),'m.');
