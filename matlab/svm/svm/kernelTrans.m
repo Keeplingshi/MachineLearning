2cab9aaf74b43cfddea14344ed1d129804147058
chenbin <chenbkeep@163.com>
2016-04-26 20:24:30 +0800

    svm

--- matlab/svm/svm/kernelTrans.m
+++
@@ -1,9 +0,0 @@
-function K = kernelTrans(X, A, k1)
-    [m, n] = size(X);
-    K = zeros(m,1);
-    for j = 1:1:m
-        deltaRow = X(j,:) - A;
-        K(j) = deltaRow * deltaRow';
-    end
-    K = exp(K./(-2*k1));
-end
