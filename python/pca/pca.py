774f4e9143e650c3a9b767f173f7f028981dbc6e
陈斌 <chenbkeep@163.com>
2016-07-20 17:50:05 +0800

    pca

--- python/pca/pca.py
+++ python/pca/pca.py
@@ -1,22 +1,33 @@
 from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
 
 def loadDataSet(fileName, delim='\t'):
     fr=open(fileName)
     stringArr=[line.strip().split(delim) for line in fr.readlines()]
-    datArr=[map(float,line) for line in stringArr]
+    datArr=[list(map(float,line)) for line in stringArr]
     return mat(datArr)
 
-# t=loadDataSet('testSet.txt')
-# print(t)
-
 def pca(dataMat,topNfeat=9999999):
+    #计算并减去原始数据集中的平均值
     meanVals=mean(dataMat,axis=0)
     meanRemoved=dataMat-meanVals
+    #计算协方差矩阵
     covMat=cov(meanRemoved,rowvar=0)
+    #计算协方差矩阵的特征值和特征向量
     eigVals,eigVects=linalg.eig(mat(covMat))
+    #将特征值从大到小排序
     eigValInd=argsort(eigVals)
     eigValInd=eigValInd[:-(topNfeat+1):-1]
+    #将数据转换到保留的特征向量构建的新空间中
     redEigVects=eigVects[:,eigValInd]
     lowDDataMat=meanRemoved*redEigVects
     reconMat=(lowDDataMat * redEigVects.T)+meanVals
     return lowDDataMat,reconMat
+
+dataM=loadDataSet('testSet.txt')
+lowDMat,recMat=pca(dataM,1)
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.scatter(dataM[:,0].flatten().A[0],dataM[:,1].flatten().A[0],marker='^',s=90)
