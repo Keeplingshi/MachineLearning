c7bb195fb67cb5b2114c748d288c5576be98438a
chenbin <chenbkeep@163.com>
2016-10-10 20:04:40 +0800

    实验二注释补充

--- tensorflow/shiyan2/shiyan2.py
+++ tensorflow/shiyan2/shiyan2.py
@@ -1,31 +1,12 @@
 import MNIST.input_data as input_data
 import tensorflow as tf
 
-
+# 为获取训练集与测试集数据
 mnist = input_data.read_data_sets("../MNIST/", one_hot=True)
 
 sess = tf.InteractiveSession()
 x = tf.placeholder("float", shape=[None, 784])
 y_ = tf.placeholder("float", shape=[None, 10])
-
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-
-sess.run(tf.initialize_all_variables())
-
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
-for i in range(1000):
-    batch = mnist.train.next_batch(50)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
 # 权重初始化
@@ -40,58 +21,71 @@ def bias_variable(shape):
 
 
 # 卷积和池化
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(a, b):
+    return tf.nn.conv2d(a, b, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+def max_pool_2x2(a):
+    return tf.nn.max_pool(a, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
+# 第一层卷积
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
-x_image = tf.reshape(x, [-1,28,28,1])
+
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 
+# 第二层卷积
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-
+# 密集链接层 fc:full conection
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
+# 图片尺寸减小到7x7,我们加入一个有1024个神经元的全连接层,用于处理整个图片。我们把池化层输出的
+# 张量reshape成一些向量,乘上权重矩阵,加上偏置,然后对其使用ReLU。
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-
+# dropout 减少过拟合 我们用一个placeholder来代表一个神经元的输出在dropout中保持不变的概率。
+# 在训练过程中启用dropout。在测试过程中关闭dropout。tensorflow的tf.nn.dropout操作除了可以屏蔽神经元的输入外,
+# 还会自动处理神经元输出值的scale.再用dropout时可以不用考虑scale.
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
+# 输出层 最后添加一个softmax层(分类)
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 
+# 评估模型
+# 为了进行训练喝评估,我们使用与之前简单的单层SoftMax神经网络模型几乎一样的代码
+# 只是这里我们会使用更加复杂的ADMA优化器来做梯度下降,在feed_dict中加入额外的参数keep_prob来控制dropout比例
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.initialize_all_variables())
+
 for i in range(20000):
     batch = mnist.train.next_batch(50)
-    if i%100 == 0:
+    if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
+            x: batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
+print("test accuracy %g" % accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
