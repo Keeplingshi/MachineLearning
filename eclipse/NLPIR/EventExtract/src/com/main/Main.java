8efbfa282c9d842b0533fda81cad105555f98d7e
chenbin <chenbkeep@163.com>
2016-10-17 17:19:40 +0800

    微博文本预处理

---
+++ eclipse/NLPIR/EventExtract/src/com/main/Main.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import com.txt.TxtDeal;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		TxtDeal.weiboTxtDeal("E:/机器学习/data/weibo/weibo.txt","E:/机器学习/data/weibo/weiboResult.txt");
+	}
+
+
+}
