f67c144cb75d7a0d796ed5e4ecabcd5f2ae788fd
chenbin <chenbkeep@163.com>
2016-11-28 11:32:56 +0800

    触发词分数统计

--- eclipse/EventExtraction/src/com/event/TriggerScore.java
+++ eclipse/EventExtraction/src/com/event/TriggerScore.java
@@ -6,31 +6,70 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.model.EventEnum;
 
 public class TriggerScore {
 
+	static final Logger LOGGER = LoggerFactory.getLogger(TriggerScorePre.class);
 	public static final String SEPORETOR = "[。,!，?]";
 	
 	public static Map<String, EventEnum> word2typeMap = TriggerLarger.getWord2typeMap();
 	
 	/**
-	 * 事件句按事件分类
+	 * 事件句按事件分类，可求出某类事件句总数
 	 */
-	public static Map<EventEnum,List<String>> eventSentenceMap=Maps.newHashMap();
+	public static Map<EventEnum,Set<String>> eventSentenceMap=Maps.newHashMap();
 	
+	/**
+	 * 触发词数目
+	 */
+	public static Map<String, Integer> triggerCounterMap = Maps.newHashMap();
+	
+	/**
+	 * 句子总数
+	 */
+	public static Integer totalNum=0;
+	
+	/**
+	 * 触发词得分
+	 */
+	private static Map<String, Double> sorceMap = Maps.newHashMap();
+	
+	
+	public static Map<String, Double> getSorceMap() {
+		return sorceMap;
+	}
+
+	public static void setSorceMap(Map<String, Double> sorceMap) {
+		TriggerScore.sorceMap = sorceMap;
+	}
+
 	public static void main(String[] args) {
 
 		String resultPath=System.getProperty("user.dir")+"/corpous/cec/CEC_Train_Corpous/cecResult.txt";
 		File file=new File(resultPath);
 		countSentence(file);
-		
+        Double result;
+        for(String word : triggerCounterMap.keySet()){
+            result = 0.0;
+            Double tfw = 1.0*triggerCounterMap.get(word)/eventSentenceMap.get(word2typeMap.get(word)).size();
+            Double idfw = Math.log((double)1.0*totalNum/triggerCounterMap.get(word))/Math.log((double)2);//Math.log(1.0*totalNum/triggerCounterMap.get(word));
+            result = tfw *idfw;
+            sorceMap.put(word, result);
+        }
+		System.out.println(sorceMap);
 	}
 
     /**
@@ -50,34 +89,27 @@ public class TriggerScore {
             	
                 Iterable<String> list = Splitter.on(Pattern.compile(SEPORETOR)).trimResults().omitEmptyStrings().split(line);
                 for(String str : list){
-                	
-                	System.out.println(str);
-                	
-//                    sentenceList.add(str);
-//                    Iterable<String> tokens = Splitter.on(Pattern.compile("(\\s)+")).trimResults().omitEmptyStrings().split(str);
-//                    for(String token : tokens){
-//                        if(word2typeMap.containsKey(token)){
-//                            increaseMap(trigger2CounterMap, token); //一个句子可以统计多个相同的词
-//                            increaseMap(type2CounterMap, word2typeMap.get(token));
-//                        }
-//                    }
-//
-//                    //Set<String> set = Sets.newHashSet(tokens);
-//                    for(String token : tokens){
-//                        if(word2typeMap.containsKey(token)){
-//                            increaseMap(trigger2SentenceCounterMap, token);   //一个句子只能统计一个相同的词
-//                        }
-//                    }
+                	totalNum++;
+                	Iterable<String> tokens = Splitter.on(Pattern.compile("(\\s)+")).trimResults().omitEmptyStrings().split(str);
+                	for(String token : tokens){
+                		if(word2typeMap.containsKey(token)){
+                			
+                			EventEnum eventEnum=word2typeMap.get(token);
+                			Set<String> sentenceSet=eventSentenceMap.get(eventEnum);
+                			if(sentenceSet==null){
+                				sentenceSet=new HashSet<>();
+                			}
+                			sentenceSet.add(str);
+                			eventSentenceMap.put(eventEnum, sentenceSet);
+                			
+                			//统计触发词数目
+                			increaseMap(triggerCounterMap,token);
+                		}
+                	}
+
                 }
                 
             }
-
-//            sentenceTotalNum = sentenceList.size();
-//            LOGGER.info("【触发词触发事件的次数】,{}", trigger2CounterMap);
-//            LOGGER.info("【触发词类型的次数】,{}", type2CounterMap);
-//            LOGGER.info("【句子个数】,{}", sentenceList.size());
-//            LOGGER.info("【触发词触发出现在句子中次数】,{}", trigger2SentenceCounterMap);
-
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -85,6 +117,20 @@ public class TriggerScore {
             e.printStackTrace();
         }
         return null;
+    }
+    
+    /**
+     * map的值递增
+     * @param map
+     */
+    private static <K> void increaseMap(Map<K, Integer> map, K key) {
+
+        if(map.containsKey(key)){
+            map.put(key, map.get(key)+1);
+        }
+        else{
+            map.put(key, 1);
+        }
     }
 	
 }
